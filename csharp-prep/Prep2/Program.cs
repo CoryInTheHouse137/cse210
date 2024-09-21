14986bfd6d7781858626a6c82644f02b5f3aa1a4
Cory Stenlund <corystenlund@Corys-MacBook-Air-2.local>
2024-09-21 13:11:22 -0600

    change function of programs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your % grade in the class? ");
+        int grade = Console.ReadLine();
+        string letter = "Not a grade, try again."
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (90 > grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (80 > grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (70 > grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congrats! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine($"Keep trying hard! You can do it next time.");
+        }
     }
 }
