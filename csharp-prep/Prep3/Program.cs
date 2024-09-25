6671ad41aa89b6edb456dfa4709d18d8ed5e26e5
Cory Stenlund <corystenlund@Corys-MacBook-Air-2.local>
2024-09-25 14:55:06 -0600

    magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        int number = -1;
+
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 11);
+
+
+        while(magicNum != number)
+        {
+            Console.Write("What is your guess? ");
+            string h = Console.ReadLine();
+            number = int.Parse(h);
+
+            if (magicNum < number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNum > number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine("Correct!");
+  
+        
+
+
+
+
     }
 }
