7ceac178b53cfe3ad925b1fa61c6e33f4d00c2b1
Cory Stenlund <corystenlund@Corys-MacBook-Air-2.local>
2024-10-29 23:20:49 -0600

    Prep inharitance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+        Assignment assignment = new Assignment("Cory Stenlund", "Geology");
+        string student = assignment.GetSummary();
+
+        Console.WriteLine(student);
+
+        Console.WriteLine("");
+        
+        MathAssignment mathAssignment = new MathAssignment("Cory Stenlund", "Fractions", "7.3", "8-19");
+        string name = mathAssignment.GetSummary();
+        string  next = mathAssignment.GetHomeworkList();
+
+        Console.WriteLine(name);
+        Console.WriteLine(next);
+
+        Console.WriteLine("");
+        
+        WritingAssignment writingAssignment = new WritingAssignment("Cory Stenlund", "History", "The Causes of World War II by Mary Waters");
+        string person = writingAssignment.GetSummary();
+        string text = writingAssignment.GetWritingInformation();
+
+        Console.WriteLine(person);
+        Console.WriteLine(text);
     }
 }
